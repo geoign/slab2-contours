d5cfef4e7bd1b9405193dd81565cb6f7f262f0dd
Fumihiko Ikegami <f.ikegami@gmail.com>
2025-12-30 15:03:39 +0900

    Delete slab2setup directory

--- slab2setup/pygmtenv.sh
+++
@@ -1,4 +0,0 @@
-echo "Creating the pygmt virtual environment:"
-conda create --name pygmt --channel conda-forge pygmt
-conda env update --file pygmt.yml
-echo 'pygmt virtual environment has been created'
